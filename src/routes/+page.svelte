f7c2082355cc88f7a570e8e74c1b8d33b97b98fb
Josh Mabry <mabry1985@gmail.com>
2022-11-02 01:26:48 -0700

    updates portrait image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-  import portrait from '$lib/assets/portrait.png';
+  import portrait from '$lib/assets/portrait.jpg';
 	import SocialIcons from '$lib/components/SocialIcons.svelte';
   import "../app.css";
 </script>
 
 <div class="w-full h-screen p-4">
   <div class="container flex flex-col lg:flex-row rounded-lg max-w-[1100px] lg:max-h-[650px] relative top-1/2 -translate-y-1/2 lg:mx-auto border-solid border border-primary">
-    <img class="p-4 lg:w-auto max-h-[33rem] self-center m-0 lg:m-12 lg:mr-0" alt="portrait of Josh Mabry" src={portrait} />
+    <img class="portrait p-4 rounded-3xl lg:w-auto max-h-[33rem] self-center m-0 lg:m-12 lg:mr-0" alt="portrait of Josh Mabry" src={portrait} />
     <div class="hero-menu flex flex-col self-center w-full lg:w-1/2 mx-2 lg:mx-12">
       <h1 class="text-primary text-center text-4xl lg:text-7xl font-serif font-light w-full my-4">Josh Mabry</h1>
       <ul class="flex flex-col items-center w-full lg:mt-6 lg:mb-8 font-sans font-light">
@@ -18,3 +18,10 @@
     </div>
   </div>
 </div>
+
+
+<style>
+  .portrait {
+    border-radius: 1.75rem;
+  }
+</style>
