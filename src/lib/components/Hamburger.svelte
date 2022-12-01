d5b05a82cea315cda54346e70d4495055b0d8386
Josh Mabry <mabry1985@gmail.com>
2022-12-01 02:09:39 -0800

    adds hamburger menu

---
+++ src/lib/components/Hamburger.svelte
@@ -0,0 +1,111 @@
+<script>
+	export let open = false;
+</script>
+
+<button
+	class="hamburger hamburger--collapse"
+	aria-label="Hamburger menu"
+	class:is-active={open}
+	on:click
+	on:click={() => (open = !open)}
+>
+	<span class="hamburger-box">
+		<span class="hamburger-inner" />
+	</span>
+</button>
+
+<style>
+	.hamburger {
+		@apply bg-primary text-primary;
+		display: inline-block;
+		cursor: pointer;
+		transition-property: opacity, filter;
+		transition-duration: 0.15s;
+		transition-timing-function: linear;
+		text-transform: none;
+		background-color: transparent;
+		border: 0;
+		margin: 0;
+		overflow: visible;
+	}
+	.hamburger:active {
+		background-color: transparent !important;
+	}
+	.hamburger.is-active .hamburger-inner,
+	.hamburger.is-active .hamburger-inner::before,
+	.hamburger.is-active .hamburger-inner::after {
+		@apply bg-primary;
+	}
+
+	.hamburger-box {
+		width: var(--layer-width, 30px);
+		height: calc(var(--layer-height, 4px) * 3 + var(--layer-spacing, 6px) * 2);
+		display: inline-block;
+		position: relative;
+	}
+
+	.hamburger-inner {
+		display: block;
+		top: 50%;
+		margin-top: var(--layer-height, 4px) / -2;
+	}
+	.hamburger-inner,
+	.hamburger-inner::before,
+	.hamburger-inner::after {
+		@apply bg-primary text-primary;
+		width: var(--layer-width, 30px);
+		height: var(--layer-height, 2px);
+		border-radius: var(--border-radius, 4px);
+		position: absolute;
+		transition-property: transform;
+		transition-duration: 0.15s;
+		transition-timing-function: ease;
+	}
+	.hamburger-inner::before,
+	.hamburger-inner::after {
+		content: '';
+		display: block;
+	}
+	.hamburger-inner::before {
+		top: calc((var(--layer-spacing, 6px) + var(--layer-height, 4px)) * -1);
+	}
+	.hamburger-inner::after {
+		bottom: calc((var(--layer-spacing, 6px) + var(--layer-height, 4px)) * -1);
+	}
+
+	/*
+    * Collapse
+    */
+	.hamburger--collapse .hamburger-inner {
+		top: auto;
+		bottom: 0;
+		transition-duration: 0.13s;
+		transition-delay: 0.13s;
+		transition-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+	}
+	.hamburger--collapse .hamburger-inner::after {
+		top: calc((var(--layer-spacing, 6px) * 2 + var(--layer-height, 4px) * 2) * -1);
+		transition: top 0.2s 0.2s cubic-bezier(0.33333, 0.66667, 0.66667, 1), opacity 0.1s linear;
+	}
+	.hamburger--collapse .hamburger-inner::before {
+		transition: top 0.12s 0.2s cubic-bezier(0.33333, 0.66667, 0.66667, 1),
+			transform 0.13s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+	}
+	.hamburger--collapse.is-active .hamburger-inner {
+		transform: translate3d(0, calc((var(--layer-spacing, 6px) + var(--layer-height, 4px)) * -1), 0)
+			rotate(-45deg);
+		transition-delay: 0.22s;
+		transition-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+	}
+	.hamburger--collapse.is-active .hamburger-inner::after {
+		top: 0;
+		opacity: 0;
+		transition: top 0.2s cubic-bezier(0.33333, 0, 0.66667, 0.33333), opacity 0.1s 0.22s linear;
+	}
+	.hamburger--collapse.is-active .hamburger-inner::before {
+		top: 0;
+		transform: rotate(-90deg);
+		transition: top 0.1s 0.16s cubic-bezier(0.33333, 0, 0.66667, 0.33333),
+			transform 0.13s 0.25s cubic-bezier(0.215, 0.61, 0.355, 1);
+	}
+</style>
---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,68 @@
+<script>
+	import { fly } from 'svelte/transition';
+	import SocialIcons from './SocialIcons.svelte';
+
+	export let open;
+</script>
+
+{#if open}
+	<div class="menu" transition:fly={{ x: 25, duration: 300 }}>
+		<nav>
+			<span
+				class="text-h4-responsive font-serif mb-2"
+				aria-hidden="true"
+				transition:fly={{ x: 25, delay: 0, duration: 300 }}>Josh Mabry</span
+			>
+			<ul>
+				{#each ['Developer', 'Designer', 'Artist', 'Blog', 'About'] as link, i}
+					<li transition:fly={{ x: 25, delay: 0, duration: 300 }}>
+						<a href="#">{link}</a>
+					</li>
+				{/each}
+			</ul>
+
+			<SocialIcons />
+		</nav>
+	</div>
+{/if}
+
+<style>
+	.menu {
+		@apply text-primary font-sans absolute -right-1 -top-1 h-full bg-dark w-screen;
+		text-align: center;
+		width: calc(100%);
+	}
+	nav {
+		@apply flex flex-col justify-center h-full rounded-lg;
+		@apply m-6;
+		height: calc(100% - 48px);
+		border: solid 2px var(--color-primary);
+	}
+	nav li {
+		@apply my-2;
+	}
+
+	nav ul {
+		@apply mb-2;
+	}
+	a {
+		@apply uppercase underline font-light py-2;
+		cursor: pointer;
+		width: max-content;
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+
+	@screen xs {
+		.menu {
+			width: calc(100vw - 6rem);
+		}
+	}
+
+	@screen sm {
+		.menu {
+			width: min(400px, 50vw);
+		}
+	}
+</style>
