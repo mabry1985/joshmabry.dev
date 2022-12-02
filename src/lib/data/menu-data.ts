1a12a84a192c440d2cdd8a6cc527020c6f73d57c
Josh Mabry <mabry1985@gmail.com>
2022-12-01 20:41:55 -0800

    adds mock menu data

--- src/lib/data/menu-data.ts
+++ src/lib/data/menu-data.ts
@@ -1,27 +1,27 @@
 export const menuData = [
-  { 
-    title: 'Developer',
-    url: '/developer',
-    alt: 'Josh Mabrys developer portfolio',
-  },
-  {
-    title: 'Designer',
-    url: '/designer',
-    alt: 'Josh Mabrys designer portfolio',
-  },
-  {
-    title: 'Artist',
-    url: '/artist',
-    alt: 'Josh Mabrys artist portfolio',
-  },
-  {
-    title: 'Blog',
-    url: '/blog',
-    alt: 'Josh Mabrys blog',
-  }, 
-  {
-    title: 'About',
-    url: '/about',
-    alt: 'Josh Mabrys about page',
-  }
+	{
+		title: 'Developer',
+		url: '/developer',
+		alt: 'Josh Mabrys developer portfolio'
+	},
+	{
+		title: 'Designer',
+		url: '/designer',
+		alt: 'Josh Mabrys designer portfolio'
+	},
+	{
+		title: 'Artist',
+		url: '/artist',
+		alt: 'Josh Mabrys artist portfolio'
+	},
+	{
+		title: 'Blog',
+		url: '/blog',
+		alt: 'Josh Mabrys blog'
+	},
+	{
+		title: 'About',
+		url: '/about',
+		alt: 'Josh Mabrys about page'
+	}
 ];
